1dd367c401258834ec246368ef7b667a6ea2f782
Lucas de Carvalho <lucassin_x@hotmail.com>
2015-08-18 15:37:19 -0300

    fields on admin

--- pizzaria/pizza/admin.py
+++ pizzaria/pizza/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-
-
 from .models import Pizza
 
 
-admin.site.register(Pizza)
+class PizzaAdmin(admin.ModelAdmin):
+    search_fields = ['flavor', 'price']
+    list_display = ['flavor', 'ingredients']
+
+admin.site.register(Pizza, PizzaAdmin)
